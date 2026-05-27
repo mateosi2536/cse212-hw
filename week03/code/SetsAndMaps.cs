eced2530fb408fabbe611c8e9464753d1b02d614
mateosi2536 <mateoszambranos@gmail.com>
2026-05-27 17:45:52 -0500

    w3

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,17 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var seen = new HashSet<string>();
+        var result = new List<string>();
+        foreach (var word in words)
+        {
+            var reversed = $"{word[1]}{word[0]}";
+            if (word[0] != word[1] && seen.Contains(reversed))
+                result.Add($"{word} & {reversed}");
+            else
+                seen.Add(word);
+        }
+        return result.ToArray();
     }
 
     /// <summary>
@@ -42,7 +51,11 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3];
+            if (degrees.ContainsKey(degree))
+                degrees[degree]++;
+            else
+                degrees[degree] = 1;
         }
 
         return degrees;
@@ -66,8 +79,12 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        var counts = new Dictionary<char, int>();
+        foreach (var c in word1.ToLower().Replace(" ", ""))
+            counts[c] = counts.GetValueOrDefault(c) + 1;
+        foreach (var c in word2.ToLower().Replace(" ", ""))
+            counts[c] = counts.GetValueOrDefault(c) - 1;
+        return counts.Values.All(v => v == 0);
     }
 
     /// <summary>
@@ -96,11 +113,8 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+        return featureCollection.Features
+            .Select(f => $"{f.Properties.Place} - Mag {f.Properties.Mag}")
+            .ToArray();
     }
 }

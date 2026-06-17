6a5be26a45ba5dd09a6348041a2c51a12cc6e8fe
mateosi2536 <mateoszambranos@gmail.com>
2026-06-17 09:25:37 -0500

    w05

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -21,7 +21,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -29,17 +29,23 @@ public class Node
             else
                 Right.Insert(value);
         }
+        // if value == Data, do nothing (no duplicates)
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
